8f7cf0153bdc44903380b764f292de44e1979383
jdlrobson <jdlrobson@gmail.com>
2016-10-21 09:41:06 -0700

    Version 0.9.1 uses webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: {
+      main: './components/index.js'
+    },
+    output: {
+      path: __dirname + '/dist/',
+      filename: 'index.js',
+      libraryTarget: 'umd'
+    },
+    externals: {
+      react: 'react',
+      reactDom: 'react-dom'
+    },
+    plugins: [
+      new ExtractTextPlugin('styles.css', { allChunks: true }),
+    ],
+    resolve: {
+      extensions: ['', '.js', '.jsx' ]
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx$/,
+          loader: 'babel?presets[]=es2015&presets=react',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel?presets[]=es2015',
+          exclude: /node_modules/
+        },
+        { test: /\.(svg)$/, loader: 'svg-url-loader' },
+        { test: /\.(gif|png|jpg)$/, loader: 'url?limit=25000' },
+        { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') },
+        { test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!less') }
+      ]
+    }
+};

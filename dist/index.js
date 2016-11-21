40854e57ac35d885df848511d9064dde24f7a994
jdlrobson <jdlrobson@gmail.com>
2016-11-20 16:01:33 -0800

    Release 1.1.5

--- dist/index.js
+++ dist/index.js
@@ -242,6 +242,8 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+	var BG_PLACEHOLDER = 'linear-gradient(transparent,transparent),url(data:image/svg+xml,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22utf-8%22%3F%3E%0A%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2056%2056%22%3E%0A%20%20%20%20%3Cpath%20fill%3D%22%23eee%22%20d%3D%22M0%200h56v56H0z%22%2F%3E%0A%20%20%20%20%3Cpath%20fill%3D%22%23999%22%20d%3D%22M36.4%2013.5H17.8v24.9c0%201.4.9%202.3%202.3%202.3h18.7v-25c.1-1.4-1-2.2-2.4-2.2zM30.2%2017h5.1v6.4h-5.1V17zm-8.8%200h6v1.8h-6V17zm0%204.6h6v1.8h-6v-1.8zm0%2015.5v-1.8h13.8v1.8H21.4zm13.8-4.5H21.4v-1.8h13.8v1.8zm0-4.7H21.4v-1.8h13.8v1.8z%22%2F%3E%0A%3C%2Fsvg%3E%0A)';
+
 	var Card = function (_Component) {
 	  _inherits(Card, _Component);
 
@@ -258,8 +260,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	      var props = this.props;
 	      var title = this.props.title;
 	      var className = this.props.className ? 'card ' + this.props.className : 'card';
+	      var meta = this.props.metaInfo;
 	      var styles = {
-	        backgroundImage: this.props.thumbnail ? 'url(' + this.props.thumbnail.source + ')' : 'none'
+	        backgroundImage: this.props.thumbnail ? 'url(' + this.props.thumbnail.source + ')' : meta ? 'none' : BG_PLACEHOLDER
 	      };
 	      var extracts = this.props.extracts.map(function (item, i) {
 	        return _react2.default.createElement(
@@ -279,7 +282,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        illustration = _react2.default.createElement(
 	          'div',
 	          { className: 'card-thumb', style: styles },
-	          this.props.metaInfo
+	          meta
 	        );
 	      }
 

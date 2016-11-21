0e1ebe3e8c26d7ddb05ace62f961ba0e4eb8faec
jdlrobson <jdlrobson@gmail.com>
2016-11-20 16:00:46 -0800

    Card's without thumbnail show placeholder image

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -4,16 +4,19 @@ import TruncatedText from '../TruncatedText'
 
 import './styles.less'
 
+const BG_PLACEHOLDER = 'linear-gradient(transparent,transparent),url(data:image/svg+xml,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22utf-8%22%3F%3E%0A%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2056%2056%22%3E%0A%20%20%20%20%3Cpath%20fill%3D%22%23eee%22%20d%3D%22M0%200h56v56H0z%22%2F%3E%0A%20%20%20%20%3Cpath%20fill%3D%22%23999%22%20d%3D%22M36.4%2013.5H17.8v24.9c0%201.4.9%202.3%202.3%202.3h18.7v-25c.1-1.4-1-2.2-2.4-2.2zM30.2%2017h5.1v6.4h-5.1V17zm-8.8%200h6v1.8h-6V17zm0%204.6h6v1.8h-6v-1.8zm0%2015.5v-1.8h13.8v1.8H21.4zm13.8-4.5H21.4v-1.8h13.8v1.8zm0-4.7H21.4v-1.8h13.8v1.8z%22%2F%3E%0A%3C%2Fsvg%3E%0A)';
+
 class Card extends Component {
   render(){
     var heading;
     var props = this.props;
     var title = this.props.title;
     var className = this.props.className ? 'card ' + this.props.className : 'card';
+    var meta = this.props.metaInfo;
     var styles = {
       backgroundImage: this.props.thumbnail ?
         'url(' + this.props.thumbnail.source + ')'
-        : 'none'
+        : meta ? 'none' : BG_PLACEHOLDER
     };
     var extracts = this.props.extracts.map( function ( item, i ) {
       return (
@@ -27,7 +30,7 @@ class Card extends Component {
       '/wiki/' + encodeURIComponent( title );
     var illustration;
     if ( this.props.thumbnail || this.props.metaInfo || this.props.showPlaceholderIllustration ) {
-      illustration =<div className="card-thumb" style={styles}>{this.props.metaInfo}</div>;
+      illustration =<div className="card-thumb" style={styles}>{meta}</div>;
     }
 
     if ( title ) {

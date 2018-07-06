bf305b263508a1d4691496ae40deb01bb425825d
jdlrobson <jdlrobson@gmail.com>
2018-07-06 10:43:00 -0700

    ErrorBox accepts children

--- components/ErrorBox/index.jsx
+++ components/ErrorBox/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './styles.less';
 
 const ErrorBox = ( props ) => {
-	return <div className="errorbox">{props.msg}</div>;
+	return <div className="errorbox">{props.msg || props.children}</div>;
 };
 
 export default ErrorBox;

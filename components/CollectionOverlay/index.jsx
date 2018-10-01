abfc70a54df53409c01feed68821bf8293e507f9
jdlrobson <jdlrobson@gmail.com>
2018-09-30 22:37:58 -0700

    Run npm run lint:fix

--- components/CollectionOverlay/index.jsx
+++ components/CollectionOverlay/index.jsx
@@ -5,20 +5,20 @@ import './styles.less';
 
 export default class CollectionOverlay extends React.Component {
 	render() {
-        var props = this.props;
-        var title = props.title || 'Add to existing collection';
-        var cancelMsg = props.cancelMsg || 'Cancel';
+		var props = this.props,
+			title = props.title || 'Add to existing collection',
+			cancelMsg = props.cancelMsg || 'Cancel';
 
-        return (
-            <Overlay {...props} className={"collection-overlay " + props.className} isDrawer={true}>
-                <h2>{title}</h2>
-                { props.onExit && <a key='edit-collection-cancel' className="cancel"
-                    onClick={props.onExit}>{cancelMsg}</a> }
-                {props.children}
-                <div className="collection-actions" key="collection-overlay-actions">
-                    {props.actions}
-                </div>
-            </Overlay>
-        );
+		return (
+			<Overlay {...props} className={'collection-overlay ' + props.className} isDrawer={true}>
+				<h2>{title}</h2>
+				{ props.onExit && <a key='edit-collection-cancel' className="cancel"
+					onClick={props.onExit}>{cancelMsg}</a> }
+				{props.children}
+				<div className="collection-actions" key="collection-overlay-actions">
+					{props.actions}
+				</div>
+			</Overlay>
+		);
 	}
 }
